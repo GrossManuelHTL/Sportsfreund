900317a6045114187116154015954852292c4389
GrossManuelHTL <m.gross@htblaleonding.onmicrosoft.com>
2025-03-26 09:16:43 +0100

    added some npm packages and some python boilerplate

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hallo Crusty');
+  res.send('Simulierte Raspberry Pi API');
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
 });
