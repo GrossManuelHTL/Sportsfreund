b1964217d3de3a7ada15bec8405f40914ebf973c
Michael Oswald <145994175+OswaldMichael2008@users.noreply.github.com>
2025-04-08 08:51:11 +0200

    fps und die sidebar weg gemacht

--- src/processing/main.py
+++ src/processing/main.py
@@ -18,14 +18,10 @@ while True:
         per = np.interp(int(length), [157, 285], [100, 0])
         poseBar = np.interp(length, [157, 285], [150, 400])
 
-        cv2.putText(img, f"{int(per)} %", (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (240, 103, 106), 3)
-        cv2.rectangle(img, (50, int(poseBar)), (85, 400), (0, 182, 255), cv2.FILLED)
-        cv2.rectangle(img, (50, 150), (85, 400), (240, 103, 106), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f"FPS: {int(fps)}", (425, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (240, 103, 106), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
