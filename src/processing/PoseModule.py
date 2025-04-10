278851dd668de4427bc3e41c6eb2dcf571c1498d
HarrerValentinHTL <v.harrer@HTBLALeonding.onmicrosoft.com>
2025-04-10 12:19:19 +0200

    The program now tells you to move farther away if you are too close

--- src/processing/PoseModule.py
+++ src/processing/PoseModule.py
@@ -41,7 +41,7 @@ class poseDetector():
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                lmList.append([id, cx, cy, lm.visibility])
                 if draw:
                     cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
         return lmList
--- src/processing/main.py
+++ src/processing/main.py
@@ -7,17 +7,30 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 pTime = 0
 detector = PoseModule.poseDetector()
+
 while True:
     success, img = cap.read()
     img = cv2.resize(img, (960, 540))
     img = detector.findPose(img)
     lmList = detector.getPosition(img)
+    visiblePoints = 0
+    screenHeight = img.shape[0]
+    screenWidth = img.shape[1]
 
-    if len(lmList) > 19:
+    if lmList and len(lmList) > 19:
         length = lmList[19][2]
         per = np.interp(int(length), [157, 285], [100, 0])
         poseBar = np.interp(length, [157, 285], [150, 400])
 
+        for point in lmList:
+            x, y = point[1], point[2]
+            visibility = point[3]
+
+            if 0 <= x <= screenWidth and 0 <= y <= screenHeight and visibility > 0.5:
+                visiblePoints += 1
+
+        if visiblePoints < 33:
+            cv2.putText(img, "Please move farther away", (50, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
